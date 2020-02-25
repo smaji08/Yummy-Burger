b58e5b45cf2c03e3fb3913341814fea05af25a1a
Augustine <smaji08@gmailcom>
2020-02-24 21:27:53 -0500

    minor stuffs

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 require('dotenv').config();
+
 var connection;
+
+
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
